1ccd7efd94586ab2b4393b81d07bb10d526178b3
sjlee0111 <icetea0111@naver.com>
2016-01-26 15:03:09 +0900

    markup index prototype

--- script.js
+++ script.js
@@ -1,52 +1,87 @@
 /*
-	RESUME
-	2016.01
+	Markup index for Frontend
+	ver 1.0
 	@icetea0111
 */
-!function($, window) {
-	$(function() {
-		resume.init();
-		resume.rateView();
-	});
 
-	var $markup = $('#markupList'); 
-	var resume = {				
+!function($, window) {
+	
+	var $markup = $('#markupList');
+
+	var player = {
 		init : function() {
-			var $sort = $('#sorting')
+			var	$sort = $('#sorting')
 			,	$sortTxt = $sort.find('button')
-			,	$sortLink = $sort.find('a').not('.index')
 			,	sortGrp = []
-			,	dev_work = []
-			,	$mask = $('.mask');
+			,	dev_work = [];
 
 			for (i=0; i<$sortTxt.length; i++) {
 				sortGrp.push($sortTxt.eq(i).attr('data-sort'));
 			}
-
+			
 			$markup.find('li').each(function() {
 				for ( i=0; i<$sortTxt.length; i++) {
 					if ($(this).find('.type').text() == $sortTxt.eq(i).text()) {
 						$(this).attr('data-type',sortGrp[i]);
 					}
+					if ($(this).find('.status').text() == $sortTxt.eq(i).text()) {
+						$(this).attr('data-status',sortGrp[i]);
+					}
+					if ($(this).find('.dev').text() == $sortTxt.eq(i).text()) {
+						$(this).attr('data-dev',sortGrp[i]);
+						//$(this).find('.dev').html($sortTxt.eq(i).find('i'))
+					}
 				}
 			});
 			
 			$('<i class="fa fa-angle-up"></i>').appendTo($markup.find('h1'));
-			$markup.find('h2').each(function() {
+			$markup.find(':header').not('h1').each(function() {
 				if ($(this).next('ul').size() > 0) {
 					$('<i class="fa fa-list"></i>').prependTo($(this));
 				}
 			});
 				
-			$('<i class="fa fa-anchor"></i>').appendTo($markup.find('.summary a'));
-			$('<i class="fa fa-pencil"></i>').appendTo($markup.find('.summary button'));
-
-			/*	
+			$('<i class="fa fa-link"></i>').appendTo($markup.find('a'));
+			$markup.find('li[data-status=issue] .status').html('<i class="fa fa-bug"></i>');
+			//$('<i class="fa fa-bug"></i>').prependTo($markup.find('li[data-status=issue] .status'));
+			
+				
 			for (k=0; k<$('.worker').find('li').size()+1; k++) {
 				dev_work[k] = $markup.find('li[data-dev=dev' + k + ']').size();
 				$('<span></span>').appendTo($sortTxt.filter('[data-sort=dev' + k + ']')).text('(' + dev_work[k] + ')');
 			}
-			*/
+
+			$markup.find('li[data-status=issue] .status').on('click', function() {
+				alert($(this).attr('title'));
+			});
+			
+			$markup.find('h1').click(function() {
+				$(this).toggleClass('close').nextUntil('h1').slideToggle(100);
+			});
+			$sort.find('h1').click(function() {
+				$(this).toggleClass('close').nextUntil('h1').not('.remote').slideToggle(100);
+			});
+			
+			$sortTxt.click(function() {
+				var sortBy = $(this).attr('data-sort');
+				$markup.removeClass('hide').find('li').removeClass('sort');
+				
+				if (!$(this).hasClass('sort')) {
+					if (sortBy == 'all') {
+						$markup.removeClass('hide');
+						$markup.find('li').removeClass('sort');
+					}
+					else {
+						$markup.addClass('hide');
+						$markup.find('li').each(function() {
+							if($(this).attr('data-type') == sortBy || $(this).attr('data-status') == sortBy || $(this).attr('data-dev') == sortBy ) {
+								$(this).addClass('sort');
+							}	
+						});
+					}
+				}
+				$(this).toggleClass('sort').parent().siblings().children().removeClass('sort');	
+			});
 
 			$('.toggler').click(function() {
 				$sort.toggleClass('show');
@@ -60,80 +95,31 @@
 			
 			$('.collapse').trigger('click');
 
-			$('.show-history').click(function() {
-				$('html,body').addClass('freeze');
-				$mask
-					.show()
-					.click(function() {
-						$('.history').removeClass('show');
-						$(this).hide();
-						$('html,body').removeClass('freeze');
-					});
+		},
 
-				$(this).parents('li').find('.history').addClass('show');
-
-			});
-
-			$sortLink.click(function(e) {
-				e.preventDefault();
-				$($(this).attr('href')).addClass('show');
-				$('.close-content').click(function() {
-					$(this).parent('.content').removeClass('show');
-				});
-			});
-
-			$markup.find('h1').click(function() {
-				$(this).toggleClass('close').nextUntil('h1').slideToggle(100);
-			});
-			$sort.find('a').click(function() {
-				//$(this).toggleClass('close').nextUntil('a').not('.remote').slideToggle(100);
-			});
-
-			$sortTxt.click(function() {
-				var sortBy = $(this).attr('data-sort');
-				$markup.removeClass('hide')
-					.find('li').removeClass('sort')
-					.find('h2').removeClass('sort');
-				
-				if (!$(this).hasClass('sort')) {
-					if (sortBy == 'all') {
-						$markup.removeClass('hide');
-						$markup.find('li').removeClass('sort');
-					}
-					else {
-						$markup.addClass('hide');
-						$markup.find('li').each(function() {
-							if($(this).attr('data-type') == sortBy) {
-								$(this).parent().prev('h2').addClass('sort');
-								$(this).addClass('sort');
-							}	
-						});
-					}
-				}
-				$(this).toggleClass('sort').parent().siblings().children().removeClass('sort');
-				
-			});
-		},		
-		rateView : function() {
+		status : function() {
 			var $total = $('#totalCount')
-			,	total_count = parseInt($markup.find('li').size());
-
-			var response_count = parseInt($markup.find('li[data-type=response]').size())
-			,	mobile_count = parseInt($markup.find('li[data-type=mobile]').size())
-			,	access_count = parseInt($markup.find('li[data-type=access]').size())
-			,	consult_count = parseInt($markup.find('li[data-type=consult]').size());
+			,	total_count = parseInt($markup.find('li').size())
+			,	complete_count = parseInt($markup.find('li[data-status=complete]').size())
+			,	issue_count = parseInt($markup.find('li[data-status=issue]').size())
+			,	work_count = parseInt($markup.find('li[data-status=work]').size())
+			,	wait_count = parseInt($markup.find('li[data-status=wait]').size())			
+			,	complete_rate = Math.floor((complete_count / total_count) * 100) + '%'
+			,	issue_rate = Math.floor((issue_count / total_count) * 100) + '%'
+			,	work_rate = Math.floor((work_count / total_count) * 100) + '%'
+			,	wait_rate = Math.floor((wait_count / total_count) * 100) + '%';
 			
-			var response_rate = Math.floor((response_count / total_count) * 100) + '%'
-			,	mobile_rate = Math.floor((mobile_count / total_count) * 100) + '%'
-			,	access_rate = Math.floor((access_count / total_count) * 100) + '%'
-			,	consult_rate = Math.floor((consult_count / total_count) * 100) + '%';
-			
-			$total.find('h1').text(total_count);
-			$('.response-count').text(response_rate).css('width',response_rate);
-			$('.mobile-count').text(mobile_rate).css('width',mobile_rate);
-			$('.access-count').text(access_rate).css('width',access_rate);
-			$('.consult-count').text(consult_rate).css('width',consult_rate);
+			$total.find('h1').text(complete_count + '/' + total_count);
+			$('.complete-count').text(complete_rate).css('width',complete_rate);
+			$('.issue-count').text(issue_rate).css('width',issue_rate);
+			$('.work-count').text(work_rate).css('width',work_rate);
+			$('.wait-count').text(wait_rate).css('width',wait_rate);
 		}
-
 	}
+
+	$(function() {
+		player.init();
+		player.status();
+	});	
+
 }(window.jQuery, window);
